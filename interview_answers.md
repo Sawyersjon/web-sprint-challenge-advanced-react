a4379be25e8404d77d33016b912ac1cf9c53768d
Jon Sawyers <jon-sawyers@lambdastudents.com>
2021-10-01 13:17:37 -0500

    finished

--- interview_answers.md
+++ interview_answers.md
@@ -3,8 +3,18 @@ Be prepared to demonstrate your understanding of this week's concepts by answeri
 
 1. What are the main differences between a stateful and a functional component?
 
+    Stateful components handle the changing of state while functional component handle the functionality of the app itself.
+
 2. When does a componentWillMount function be called? What about a componentWillUpdate?
 
-3. Define stateful logic.
+    The componentWillMount is called when the initial render is ran.  and componentWillUpdate will run when the state in that component is updated.
+
+3. Define stateful logic. 
+
+    It's the creation of state and the changing of state
 
 4. What are the three step of creating a successful test? What is done in each phase?
+
+    Arrange: Setup the react component we are testing.
+    Act: Execute our behavior (is there is one).
+    Assert: Extract the response element and check (if necessary) that it is what we expect it to be.
--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,10 +1,33 @@
 import React from "react";
 import MutationObserver from 'mutationobserver-shim';
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
+import App from '../App'
+import userEvent from "@testing-library/user-event"
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("renders without errors", () => {});
+test("renders without errors", () => {
+    render(<App />)
+});
 
-test("shows success message on submit with form details", () => {});
+test("shows success message on submit with form details", () => {
+    render(<CheckoutForm />);
+    const firstNameInput = screen.getByLabelText("First Name:");
+    userEvent.type(firstNameInput, "Jon");
+    const lastNameInput = screen.getByLabelText("Last Name:");
+    userEvent.type(lastNameInput, "Sawyers");
+    const addressInput = screen.getByLabelText("Address:");
+    userEvent.type(addressInput, "100 South St");
+    const cityInput = screen.getByLabelText("City:");
+    userEvent.type(cityInput, "Monroe Center");
+    const stateInput = screen.getByLabelText("State:");
+    userEvent.type(stateInput, "IL");
+    const zipInput = screen.getByLabelText("Zip:");
+    userEvent.type(zipInput, "61052");
+    const button = screen.getByRole("button")
+    userEvent.click(button)
+
+    const successMessage = screen.getByText("You have ordered some plants! Woo-hoo!")
+    expect(successMessage).toBeInTheDocument();
+});
